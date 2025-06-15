3707e89f5db0bf884a3cd40170c56642ce6274dd
Adrian <adrian31dg@gmail.com>
2025-06-15 18:26:01 +0800

    Navigation Bar Done

---
+++ app/components/navbar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    // navbar
+    <div className="z-50 flex w-full p-4 bg-[#6A6B70]/20 fixed top-0 left-0 backdrop-blur-md justify-between items-center">
+      {/* logo */}
+      <div className="h-10 w-10 bg-[#6A6B70] rounded-xl opacity-100" />
+      {/* links */}
+      <div className="flex text-white gap-5 pe-2">
+        <div className="flex h-10 w-25 bg-[#131316] rounded-full border-1 border-[#6A6B70] transition-all items-center justify-center hover:bg-[#26262C] duration-300">
+          <Link href="/skills">Skills</Link>
+        </div>
+        <div className="flex h-10 w-25 bg-[#131316] rounded-full border-1 border-[#6A6B70] transition-all items-center justify-center hover:bg-[#26262C] duration-300">
+          <Link href="/about">Projects</Link>
+        </div>
+        <div className="flex h-10 w-25 bg-[#131316] rounded-full border-1 border-[#6A6B70] transition-all items-center justify-center hover:bg-[#26262C] duration-300">
+          <Link href="/about">About</Link>
+        </div>
+        <div className="flex h-10 w-25 bg-[#131316] rounded-full border-1 border-[#6A6B70] transition-all items-center justify-center hover:bg-[#26262C] duration-300">
+          <Link href="/contact">Contact</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Navbar from "@/app/components/navbar";
+import Hero from "@/app/components/hero";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar />
+      <Hero />
+      <div className="h-1000 bg-[#121212]"></div>
     </div>
   );
 }
