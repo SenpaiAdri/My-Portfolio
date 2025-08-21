8ce90fe236ec22666fbbef325e8d4f232a5d74bf
Adrian <adrian31dg@gmail.com>
2025-08-21 21:22:39 +0800

    added meteors animation and fixed meteors size

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Meteors } from './ui/meteors'
 
 const hero = () => {
   return (
-    <div>
-      
+    <div className='relative w-full h-screen'>
+      <div className='bg-grey w-full h-100'>
+
+      </div>
+      <Meteors number={40} />
     </div>
   )
 }
