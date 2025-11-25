e49f9ba47af63a577aa6caa848ef4c7388a45293
SenpaiAdri <adrian31dg@gmail.com>
2025-11-26 03:09:06 +0800

    Refactor hero and navbar components for improved layout and styling; updated meteors count to 80.

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,15 +1,66 @@
-import React from 'react'
-import { Meteors } from './ui/meteors'
+import React from "react";
+import { Meteors } from "./ui/meteors";
 
 const hero = () => {
   return (
-    <div className='relative w-full h-screen'>
-      <div className='bg-grey w-full h-100'>
-
+    <div className="relative w-full h-screen mt-20">
+      {/* Top Right Content */}
+      <div>
+        {/* Top Right Content Container */}
+        <div className="flex flex-wrap items-start justify-end gap-10 mt-25 px-10">
+          {/* Quote */}
+          <div className="border border-[#6A6B70] rounded-3xl p-8 backdrop-blur-md">
+            <h1 className="text-2xl italic text-right">
+              "Building Practical Solutions for a<br />
+              Better Future"
+            </h1>
+          </div>
+          {/* Name and Description */}
+          <div className="border border-[#6A6B70] rounded-3xl p-8 backdrop-blur-md text-right">
+            <h1 className="text-5xl font-bold italic pb-5">
+              I&apos;m Adrian M. De Guzman
+            </h1>
+            <p className="text-gray-200 text-2xl italic">
+              A Computer Science Student Major <br />
+              In Mobile Programming
+            </p>
+          </div>
+        </div>
       </div>
-      <Meteors number={40} />
+      {/* Center Content */}
+      <div className="bg-grey w-full h-100 flex flex-col items-center justify-center">
+        <h1 className="text-7xl font-bold">Welcome to</h1>
+        <h1 className="text-4xl">My Portfolio</h1>
+      </div>
+      {/* Bottom Right Content */}
+      <div>
+        <div className="flex flex-wrap items-end justify-start gap-10 mt-25 px-10">
+          {/* Achievements */}
+          <div className="border border-[#6A6B70] rounded-3xl p-8 backdrop-blur-md">
+            <h1 className="text-5xl font-bold italic text-left pb-5">
+              One of the developers
+              <br />
+              of Pasada
+            </h1>
+            <p className="text-gray-200 text-2xl italic">
+              An AI-Powered Ride-Hailing and Fleet Management Platform for
+              <br />
+              Modernized Jeepneys Services
+            </p>
+          </div>
+          {/* Skills */}
+          <div className="border border-[#6A6B70] rounded-3xl p-8 backdrop-blur-md text-left">
+            <h1 className="text-4xl italic mb-2">Skills</h1>
+            <p className=" text-gray-300 text-2xl">
+              Dart | Flutter | PostgreSQL <br /> Supabase | Figma
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Meteors */}
+      <Meteors number={80} />
     </div>
-  )
-}
+  );
+};
 
-export default hero
+export default hero;
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,21 +4,21 @@ import React from "react";
 const Navbar = () => {
   return (
     // navbar
-    <div className="z-50 flex w-full p-4 bg-[#6A6B70]/20 fixed top-0 left-0 backdrop-blur-md justify-between items-center">
+    <div className="z-50 flex w-full p-4 fixed top-0 left-0 justify-center items-center">
       {/* logo */}
-      <div className="h-10 w-10 bg-[#6A6B70] rounded-xl opacity-100" />
+      {/* <div className="h-10 w-10 bg-[#6A6B70] rounded-xl opacity-100" /> */}
       {/* links */}
       <div className="flex text-white gap-5 pe-2">
-        <div className="flex h-10 w-25 bg-[#131316] rounded-full border-1 border-[#6A6B70] transition-all items-center justify-center hover:bg-[#26262C] duration-300">
+        <div className="flex h-10 w-25 bg-[#131316]/10 backdrop-blur-md rounded-full border-1 border-[#6A6B70] transition-all items-center justify-center hover:bg-[#26262C] duration-300">
           <Link href="/skills">Skills</Link>
         </div>
-        <div className="flex h-10 w-25 bg-[#131316] rounded-full border-1 border-[#6A6B70] transition-all items-center justify-center hover:bg-[#26262C] duration-300">
+        <div className="flex h-10 w-25 bg-[#131316]/10 backdrop-blur-md rounded-full border-1 border-[#6A6B70] transition-all items-center justify-center hover:bg-[#26262C] duration-300">
           <Link href="/about">Projects</Link>
         </div>
-        <div className="flex h-10 w-25 bg-[#131316] rounded-full border-1 border-[#6A6B70] transition-all items-center justify-center hover:bg-[#26262C] duration-300">
+        <div className="flex h-10 w-25 bg-[#131316]/10 backdrop-blur-md rounded-full border-1 border-[#6A6B70] transition-all items-center justify-center hover:bg-[#26262C] duration-300">
           <Link href="/about">About</Link>
         </div>
-        <div className="flex h-10 w-25 bg-[#131316] rounded-full border-1 border-[#6A6B70] transition-all items-center justify-center hover:bg-[#26262C] duration-300">
+        <div className="flex h-10 w-25 bg-[#131316]/10 backdrop-blur-md rounded-full border-1 border-[#6A6B70] transition-all items-center justify-center hover:bg-[#26262C] duration-300">
           <Link href="/contact">Contact</Link>
         </div>
       </div>
