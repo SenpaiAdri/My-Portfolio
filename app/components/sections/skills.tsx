24bcca571495d20fad07677ff66d96c17b63c893
SenpaiAdri <adrian31dg@gmail.com>
2025-12-22 14:45:51 +0800

    initialized skills section, gonna save things before moving to a new distro :>

---
+++ app/components/sections/skills.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center">
+      <h1>Skills</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/app/components/sections/navbar";
 import Hero from "@/app/components/sections/hero";
 import Projects from "@/app/components/sections/projects";
+import Skills from "@/app/components/sections/skills";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Projects />
-
+      <Skills />
       <div className="h-300 bg-[#121212] items-center justify-center flex">
         <h1 className="text-white text-4xl font-bold text-center">This part is still under construction</h1>
       </div>
