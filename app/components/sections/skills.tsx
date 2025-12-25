ba68fa91c2e9f233f65237cdcb6ced46ee10b54d
SenpaiAdri <adrian31dg@gmail.com>
2025-12-26 01:07:55 +0800

    adjusted the layout of skills to my likings, adjusted the order of images of the projects in projects.ts

--- app/components/sections/skills.tsx
+++ app/components/sections/skills.tsx
@@ -6,28 +6,30 @@ const Skills = () => {
   const skillList = skills.map((skill) => (
     <div
       key={skill.type}
-      className="bg-[#131316] rounded-3xl w-content flex flex-col items-top"
+      className="bg-[#131316] rounded-3xl min-w-60 p-5 gap-4 flex flex-col shadow-xl 
+      shadow-black/20 hover:shadow-black/50 transition-all duration-300 hover:scale-105"
     >
-      <h1 className="text-white text-2xl font-bold pb-2">{skill.type}</h1>
-      {skill.list.map((item, index) => (
-        <div
-          key={`${skill.type}-${item}-${index}`}
+      <h1 className="text-white text-xl py-2 text-center">{skill.type}</h1>
+      <div className="flex flex-wrap justify-center max-w-55 gap-4">
+        {skill.list.map((item, index) => (
+          <div
+            key={`${skill.type}-${item}-${index}`}
           className="text-white text-xl flex items-center justify-center p-2"
-        >
-          {/* <h1>{item}</h1> */}
-          <Image src={skill.image[index]} alt={item} width={40} height={40} className="mr-2" />
-        </div>
-        
-      ))}
+          >
+            {/* <h1>{item}</h1> */}
+            <Image src={skill.image[index]} alt={item} width={50} height={50} className="m-2 hover:scale-120 transition-all duration-300" />
+          </div> 
+        ))}
+      </div>
     </div>
   ));
 
   return (
     <div id="skills">
       <div className="flex flex-col items-center justify-center py-10">
-        <h1 className="text-3xl font-bold text-center md:text-5xl lg:text-5xl text-white">Skills</h1>
+        <h1 className="text-3xl font-bold text-center mt-10 md:text-5xl lg:text-5xl text-white">Skills</h1>
         {/* Skills Container */}
-        <div className="bg-[#26262C] rounded-3xl p-8 m-10 w-content flex flex-wrap gap-4">
+        <div className="bg-[#26262C] rounded-4xl p-10 my-50 w-content flex flex-wrap gap-8 shadow-xl shadow-[#26262C]/10 hover:shadow-[#26262C]/20 transition-all duration-300">
             {skillList}
         </div>
       </div>
--- app/data/project.ts
+++ app/data/project.ts
@@ -1,7 +1,6 @@
 export interface Project {
   id: number;
   title: string;
-  category: string;
   description: string;
   imageColor: string; // Temporary placeholder until you have real images
   logo?: string; // Path to the project logo
@@ -15,7 +14,6 @@ export const projectsData: Project[] = [
   {
     id: 1,
     title: "Pasada Driver Side",
-    category: "Mobile App",
     description:
       "A Location-tracking, Booking Manager and Quota Monitoring App for the Drivers and Conductors of Modernized Jeepneys",
     imageColor: "bg-red-500",
@@ -25,16 +23,16 @@ export const projectsData: Project[] = [
     link: "",
     images: [
       "/pasada_driver_screenshots/pasada_driver_activity_page_view.jpg",
-      "/pasada_driver_screenshots/pasada_driver_activity_page_view2.jpg",
       "/pasada_driver_screenshots/pasada_driver_dropoff_view.jpg",
-      "/pasada_driver_screenshots/pasada_driver_dropoff_view2.jpg",
       "/pasada_driver_screenshots/pasada_driver_export_view.jpg",
-      "/pasada_driver_screenshots/pasada_driver_export_view2.jpg",
-      "/pasada_driver_screenshots/pasada_driver_home_driving_view.jpg",
+      "/pasada_driver_screenshots/pasada_driver_activity_page_view2.jpg",
+      "/pasada_driver_screenshots/pasada_driver_dropoff_view2.jpg",
       "/pasada_driver_screenshots/pasada_driver_home_view.jpg",
       "/pasada_driver_screenshots/pasada_driver_main_screen.jpg",
       "/pasada_driver_screenshots/pasada_driver_manual_booking_view.jpg",
+      "/pasada_driver_screenshots/pasada_driver_export_view2.jpg",
       "/pasada_driver_screenshots/pasada_driver_profile_view.jpg",
+      "/pasada_driver_screenshots/pasada_driver_home_driving_view.jpg",
       "/pasada_driver_screenshots/pasada_driver_select_route_view.jpg",
       "/pasada_driver_screenshots/pasada_driver_start_driving_view.jpg",
       "/pasada_driver_screenshots/pasada_driver_undo_view.jpg",
@@ -43,7 +41,6 @@ export const projectsData: Project[] = [
   {
     id: 2,
     title: "AI Blog Post",
-    category: "Web App",
     description:
       "An AI-Powered Blogging Channel for Latest Tech News and Updates",
     imageColor: "bg-blue-500",
@@ -54,7 +51,6 @@ export const projectsData: Project[] = [
   {
     id: 3,
     title: "To Be Announced",
-    category: "Coming Soon",
     description:
       "Next project is still on the back of my mind and will be announced soon",
     imageColor: "bg-purple-500",
